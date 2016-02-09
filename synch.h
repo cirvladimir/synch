3c1f55633be287fdbca0c8d233979cf1d4ecc7aa
Vladimir <v_korukov@hotmail.com>
2016-02-09 13:07:38 -0800

    Initial commit

---
+++ synch.h
@@ -0,0 +1,6 @@
+/** This file will have syncronization abilities for multiple processes.
+  * A process can issue commands, then other processes may have to respond
+  * when that command is completed. (c++)
+  */
+
+
